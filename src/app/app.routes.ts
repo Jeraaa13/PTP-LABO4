647d40233631de5e9a9a638427b22b7ca4200e38
Gerardo <gerardo.m.toranzo@gmail.com>
2024-09-11 19:10:23 -0300

    Sprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuienSoyComponent } from './pages/quien-soy/quien-soy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: 'login', component: LoginComponent },
+                                { path: 'home', component: HomeComponent },
+                                { path: 'quien-soy', component: QuienSoyComponent },
+                                { path: '',  redirectTo: 'home', pathMatch: 'full' },
+                              ];
---
+++ src/app/pages/quien-soy/quien-soy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-quien-soy',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './quien-soy.component.html',
+  styleUrl: './quien-soy.component.css'
+})
+export class QuienSoyComponent {
+
+}
